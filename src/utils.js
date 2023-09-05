3d30c132ed3379c318cd4d7fd022e4c567de8967
Kehinde13 <balogunkehinde3@gmail.com>
2023-09-05 14:01:46 -0700

    handle scenario where end is not specified

--- src/utils.js
+++ src/utils.js
@@ -1,27 +1,25 @@
-/* export const range = (start, end, step = 1) => {
+ export const range = (start, end, step = 1) => {
   let output = [];
   // solve this range utility
 
   // handle scenario where end is not specified range(5) - [0, 1, 2, 3, 4]
   // a conditional check for end
-
-  for (let i = start; i < end; i += step) {
-    // loop code
-  }
-
-  return output;
-}; */
-
-export const range = (start, end) => {
-  let output = [];
- 
-  for(let i = 0; i < start; i++){
-    let row = []
-    for(let j = 0; j < end; j++){
-      row.push("#")
+  if(end !== undefined){
+    for(let i = 0; i < start; i += step){
+      let row = []
+      for(let j = 0; j < end; j += step){
+        row.push(null)
+      }
+      output.push(row)
     }
-    output.push(row)
+  } else {
+     for(let i = 0; i < start; i += step){
+       output.push(null)
+     }
   }
+  
 
   return output;
-};
+}; 
+
+
