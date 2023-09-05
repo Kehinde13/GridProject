e6305960a64106712c67349aed66af9f811301e6
Kehinde13 <balogunkehinde3@gmail.com>
2023-09-05 10:51:02 -0700

    Initial commit

---
+++ src/utils.js
@@ -0,0 +1,27 @@
+/* export const range = (start, end, step = 1) => {
+  let output = [];
+  // solve this range utility
+
+  // handle scenario where end is not specified range(5) - [0, 1, 2, 3, 4]
+  // a conditional check for end
+
+  for (let i = start; i < end; i += step) {
+    // loop code
+  }
+
+  return output;
+}; */
+
+export const range = (start, end) => {
+  let output = [];
+ 
+  for(let i = 0; i < start; i++){
+    let row = []
+    for(let j = 0; j < end; j++){
+      row.push("#")
+    }
+    output.push(row)
+  }
+
+  return output;
+};
